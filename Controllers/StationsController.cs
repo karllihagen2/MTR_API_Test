98a49784de2da6077cad2963610cc0b684e527e4
karllihagen2 <karl.lihagen@ffcg.se>
2022-03-31 08:23:08 +0200

    Add project files.

---
+++ Controllers/StationsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MTR.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Test.Controllers
+{
+
+    public class StationsController : ControllerBase
+    {
+        private readonly IStationService _stationService;
+
+        public StationsController(IStationService stationService)
+        {
+            _stationService = stationService;
+        }
+        [HttpPost]
+        [Route("v1/station/getstation/{firstCoordinate}")]
+        public async Task<IActionResult> GetStation(string firstCoordinate)
+        {
+            var result = await _stationService.GetStationBasedOnCoordinate(firstCoordinate);
+
+            return Ok(result);
+        }
+    }
+}
